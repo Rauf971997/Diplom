361e19758e8c00853d182d6b2674455d61476c6c
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-17 09:43:35 +0400

    Define initial pages for further work

--- src/redux/features/playerSlice.js
+++ src/redux/features/playerSlice.js
@@ -27,6 +27,39 @@ const playerSlice = createSlice({
       state.currentIndex = action.payload.i;
       state.isActive = true;
     },
+
+    nextSong: (state, action) => {
+      if (state.currentSongs[action.payload]?.track) {
+        state.activeSong = state.currentSongs[action.payload]?.track;
+      } else {
+        state.activeSong = state.currentSongs[action.payload];
+      }
+
+      state.currentIndex = action.payload;
+      state.isActive = true;
+    },
+
+    prevSong: (state, action) => {
+      if (state.currentSongs[action.payload]?.track) {
+        state.activeSong = state.currentSongs[action.payload]?.track;
+      } else {
+        state.activeSong = state.currentSongs[action.payload];
+      }
+
+      state.currentIndex = action.payload;
+      state.isActive = true;
+    },
+
+    playPause: (state, action) => {
+      state.isPlaying = action.payload;
+    },
+
+    selectGenreListId: (state, action) => {
+      state.genreListId = action.payload;
+    },
   },
 });
 
+export const { setActiveSong, nextSong, prevSong, playPause, selectGenreListId } = playerSlice.actions;
+
+export default playerSlice.reducer;
