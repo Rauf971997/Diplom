fa28fbb83344b8fb8c1fdda93b2aeb5136c09d52
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-17 14:50:22 +0400

    checking how Songcards should be displayed

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,12 +4,18 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSongs } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => (
-  <div
-    className="flex flex-col w-[250px]
-   p-4 bg-white/5 bg-opacity-80
-    backdrop-blur-sm animate animate-slideup rounded-lg cursor-pointer"
-  ></div>
-);
+const SongCard = ({ song, i }) => {
+
+  const activeSong = "test";
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong ?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export default SongCard;
