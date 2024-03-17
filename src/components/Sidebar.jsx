732d40deb8f28169f5c459e4f50e2f784924f454
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-17 17:34:52 +0400

    Start work on sidebar component and added initial styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,18 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from "react";
+import { NavLink} from "react-router-dom";
+import { RiCloseLine} from "react-icons/ri";
+// my logo import this here  import logo from "./"
+import links from "../data/links"
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+
+    </div>
+  );
+};
 
 export default Sidebar;
