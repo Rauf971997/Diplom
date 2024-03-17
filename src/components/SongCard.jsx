99184831c078b0bfb9cedff2c7f74b611563094f
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-17 14:34:04 +0400

    Define initial structure for songCard components

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,15 @@
-const SongCard = () => (
-  <div>SongCard</div>
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSongs } from "../redux/features/playerSlice";
+
+const SongCard = ({ song, i }) => (
+  <div
+    className="flex flex-col w-[250px]
+   p-4 bg-white/5 bg-opacity-80
+    backdrop-blur-sm animate animate-slideup rounded-lg cursor-pointer"
+  ></div>
 );
 
 export default SongCard;
