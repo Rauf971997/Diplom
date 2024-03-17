3a2a416d2cc71fe96abf0ed0374bd992b5145609
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-17 15:15:04 +0400

    Added functionality for showing title and subtitle and added initial styles

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -15,6 +15,21 @@ const SongCard = ({ song, i }) => {
         </div>
         <img src={song.images?.coverart} alt="song_img"/>
       </div>
+
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>
+          {song.title}
+          </Link>
+        </p>
+
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
+          {song.subtitle}
+          </Link>
+        </p>
+
+      </div>
     </div>
   );
 };
