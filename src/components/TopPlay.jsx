9607130b96be1e890de13e1cfae048ca09a0dd3a
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-18 16:45:04 +0400

    Added styles for topplay components

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode} from "swiper";
+import FreeMode from "swiper";
 
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong} from "../redux/features/playerSlice";
@@ -15,10 +15,10 @@ import 'swiper/css/free-mode';
 
 const TopPlay = () => {
 const dispatch = useDispatch();
-const [activeSong, isPlaying] = useSelector((state)=>state.player);
+const {activeSong, isPlaying} = useSelector((state)=>state.player);
 
 const { data} =useGetTopChartsQuery();
-const divRef =useRef(null);
+const divRef = useRef(null);
 
 useEffect(()=>{
   divRef.current.scrollIntoView({ behavior : 'smooth'});
@@ -30,8 +30,8 @@ const handlePauseClick = () => {
   dispatch(playPause(false));
 };
 
-const handlePlayClick = () => {
-  dispatch(setActiveSongs({ song, data, i }));
+const handlePlayClick = (song, data, i) => {
+  dispatch(setActiveSong({ song, data, i }));
   dispatch(playPause(true));
 };
 
@@ -39,9 +39,9 @@ return (
  <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
   <div className="w-full flex flex-col">
    <div className="flex flex-row justify-between items-center">
-       <h2>Top Charts</h2>
+       <h2 className="text-white font-bold  text-2xl">Top Charts</h2>
        <Link  to="/top-charts">
-         <p>See more</p>
+         <p className="text-gray-300 text-base cursor-pointer">See more</p>
        </Link>
    </div>
   </div>
