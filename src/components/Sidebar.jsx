72b3027331b9b9676a6d70c4ca91b68cbcf490e0
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-18 12:22:00 +0400

    Continue  work on Sidebar components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,13 +4,21 @@ import { RiCloseLine} from "react-icons/ri";
 import logo from "../assets/logo.png"
 import links from "../data/links"
 
+const NavLinks = () =>{
+<div className="mt-10">
+   {links.map((item)=>{
+    <NavLink />
+   })}
+</div>
+}
+
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
       <img src={logo} alt="logo" className="w-full h-20 object-contain" />
-
+      <NavLinks />
     </div>
   );
 };
