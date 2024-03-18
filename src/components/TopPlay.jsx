0c160aeb004549269e4e498d1cea3fa718115d25
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-18 18:05:54 +0400

    Added initial styles for Top Artists section

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -5,63 +5,73 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import FreeMode from "swiper";
 
 import PlayPause from "./PlayPause";
-import { playPause, setActiveSong} from "../redux/features/playerSlice";
-import { useGetTopChartsQuery} from "../redux/services/shazamCore";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
-import 'swiper/css';
-import 'swiper/css/free-mode';
+import "swiper/css";
+import "swiper/css/free-mode";
 
-const TopChartCard=({song, i}) =>{
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-   {song.title}
-  </div>
-}
-
+const TopChartCard = ({ song, i }) => {
+  return (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      {song.title}
+    </div>
+  );
+};
 
 const TopPlay = () => {
-const dispatch = useDispatch();
-const {activeSong, isPlaying} = useSelector((state) => state.player);
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-const { data } =useGetTopChartsQuery();
-const divRef = useRef(null);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
 
-useEffect(()=>{
-  divRef.current.scrollIntoView({ behavior : 'smooth'});
-});
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  });
 
-const topPlays = data?.slice(0, 5);
+  const topPlays = data?.slice(0, 5);
 
-const handlePauseClick = () => {
-  dispatch(playPause(false));
-};
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
 
-const handlePlayClick = () => {
-  dispatch(setActiveSong({ song, data, i }));
-  dispatch(playPause(true));
-};
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
-return (
- <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
-  <div className="w-full flex flex-col">
-   <div className="flex flex-row justify-between items-center">
-       <h2 className="text-white font-bold  text-2xl">Top Charts</h2>
-       <Link  to="/top-charts">
-         <p className="text-gray-300 text-base cursor-pointer">See more</p>
-       </Link>
-   </div>
-   <div className="mt-4 flex flex-col gap-1">
-    {topPlays?.map((song, i)=>(
-      <TopChartCard 
-      key={song.key}
-      song={song}
-      i={i}
-      />
-    ))}
-   </div>
-  </div>
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold  text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard key={song.key} song={song} i={i} />
+          ))}
+        </div>
+      </div>
 
- </div>
-)
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold  text-2xl">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        
+      </div>
+    </div>
+  );
 };
 
 export default TopPlay;
