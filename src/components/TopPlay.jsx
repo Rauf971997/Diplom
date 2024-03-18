e04a6835aecb24be987136fcd40dfd12e906b99c
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-18 18:32:31 +0400

    Added artists img for charts component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -14,7 +14,12 @@ import "swiper/css/free-mode";
 const TopChartCard = ({ song, i }) => {
   return (
     <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-      {song.title}
+      <h3 className="font-bold text-base text-white mr-3">{i+1}</h3>
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <img src={song?.images?.coverart} alt={song?.title}
+        className="w-20 h-20 rounded-lg"
+        />
+      </div>
     </div>
   );
 };
