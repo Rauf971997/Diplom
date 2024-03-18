ad5b9db27f7454cdc82919c786a473aa65875043
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-18 17:50:25 +0400

    Define TopChartCard function for show 5 top charts and added initial styles

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,13 +11,18 @@ import { useGetTopChartsQuery} from "../redux/services/shazamCore";
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
+const TopChartCard=({song, i}) =>{
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+   {song.title}
+  </div>
+}
 
 
 const TopPlay = () => {
 const dispatch = useDispatch();
-const {activeSong, isPlaying} = useSelector((state)=>state.player);
+const {activeSong, isPlaying} = useSelector((state) => state.player);
 
-const { data} =useGetTopChartsQuery();
+const { data } =useGetTopChartsQuery();
 const divRef = useRef(null);
 
 useEffect(()=>{
@@ -30,7 +35,7 @@ const handlePauseClick = () => {
   dispatch(playPause(false));
 };
 
-const handlePlayClick = (song, data, i) => {
+const handlePlayClick = () => {
   dispatch(setActiveSong({ song, data, i }));
   dispatch(playPause(true));
 };
@@ -43,6 +48,15 @@ return (
        <Link  to="/top-charts">
          <p className="text-gray-300 text-base cursor-pointer">See more</p>
        </Link>
+   </div>
+   <div className="mt-4 flex flex-col gap-1">
+    {topPlays?.map((song, i)=>(
+      <TopChartCard 
+      key={song.key}
+      song={song}
+      i={i}
+      />
+    ))}
    </div>
   </div>
 
