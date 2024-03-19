0f8c43be1a6ad6873035ff25ad8a25777f9745ce
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-19 15:37:49 +0400

    delay work with details component due error show music text latest fixed  bug

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
@@ -6,29 +7,32 @@ import { setActiveSong, playPause } from "../redux/features/playerSlice";
 import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
-    const dispatch= useDispatch();
-   const { songid} =useParams();
-   const { activeSong, isPlaying } = useSelector((state) => state.player);
-   const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
-    
-
-    return (
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+ 
+  return (
     <div className="flex flex-col">
-     <DetailsHeader artistId="" songData={songData}/>
+      <DetailsHeader artistId="" songData={songData} />
 
-     <div className="mb-10">
-   <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+          <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
 
-   <div className="mt-5">
-   {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i)=>(
-    <p className="text-gray-400 text-base my-1">{line}</p>
-)):<p className="text-gray-400 text-base my-1">Sorry, no lyrics found!</p>}
+        <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData?.sections[1]?.text.map((line, i) => (
+              <p  className="text-gray-400 text-base my-1">{line}</p>
+            ))
+            : (
+              <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
+            )}
+        </div>
+      </div>
 
-   </div>
-     </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default SongDetails;
-
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -9,15 +9,15 @@ const options = {
 };
 
 
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-.then((response) => response.json())
-.then((response) => console.log(response))
-.catch((err) => console.error(err));
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+// .then((response) => response.json())
+// .then((response) => console.log(response))
+// .catch((err) => console.error(err));
 
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', 'cf8c817e16msh0ed12b14b5f6f75p196d73jsnce6c947bd5b0');
             headers.set('X-RapidAPI-Host', 'shazam-core.p.rapidapi.com');
@@ -25,16 +25,22 @@ export const shazamCoreApi = createApi({
         }
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({
-            query: () => '/charts/world',
-        }),
-        getSongDetails: builder.query({ query: ( {songid} ) => `/tracks/details?track_id=${songid}` }),
-    }),
-});
-
-export const {
-    useGetTopChartsQuery,
-    useGetSongDetailsQuery,
-} = shazamCoreApi
-
-
+        getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `v2/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+      }),
+    });
+    
+    export const {
+      useGetTopChartsQuery,
+      useGetSongsByGenreQuery,
+      useGetSongsByCountryQuery,
+      useGetSongsBySearchQuery,
+      useGetArtistDetailsQuery,
+      useGetSongDetailsQuery,
+      useGetSongRelatedQuery,
+    } = shazamCoreApi;
