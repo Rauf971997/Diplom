91276f6bbbb4abf383652fde5056cec64748f3de
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-19 19:08:26 +0400

    Define relatedsongs component and added functionallity

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DetailsHeader = ({ artistId, artistData, songData }) => (
   <div className="relative w-full flex flex-col">
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
 import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
@@ -62,7 +62,5 @@ const SongDetails = () => {
     </div>
   );
 };
-
-
 
 export default SongDetails;
