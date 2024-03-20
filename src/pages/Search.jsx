64a3a8fded8ff0e828ed59c7fbf6a3cc9b84cccf
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-20 15:09:54 +0400

    define serch component for added functionlity search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,16 @@
-const Search = () => (
-  <div>Search</div>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-export default Search;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCore';
+
+const Search = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Search
