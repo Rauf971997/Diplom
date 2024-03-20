18d343d8bfc5b6ecdd6b5065bf510ccb7c48ac47
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-20 14:41:49 +0400

    add imports and hooks for further work on searchBar component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,25 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default Searchbar;
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Searchbar
+
