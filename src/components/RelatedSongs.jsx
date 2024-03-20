187bf41c7263ca376c1d0d7d5e2c30301cb2274b
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-20 11:41:52 +0400

    Added return for relatedSongs component for correctness operation

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
 import SongBar from './SongBar';
-const RelatedSongs = ({ data, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className="flex flex-col">
-    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
 
-    <div className="mt-6 w-full flex flex-col">
-      {data?.map((song, i) => (
-        <SongBar
-          key={`${artistId}-${song.key}-${i}`}
-          song={song}
-          i={i}
-          artistId={artistId}
-          isPlaying={isPlaying}
-          activeSong={activeSong}
-          handlePauseClick={handlePauseClick}
-          handlePlayClick={handlePlayClick}
-        />
-      ))}
+const RelatedSongs = ({ data, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+
+      <div className="mt-6 w-full flex flex-col">
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${artistId}-${song.key}-${i}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-
-export default RelatedSongs
+export default RelatedSongs;
