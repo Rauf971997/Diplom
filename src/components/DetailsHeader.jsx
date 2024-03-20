7d7bf1e82a9c330d0ec7b365ea920201509592ae
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-20 13:19:14 +0400

    defined functionlity to display the music of a given singer

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const DetailsHeader = ({ artistId, artistData, songData }) => {
-const artist=artistData?.artists[artistId]?.attributes
-
   return (
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
@@ -12,9 +10,10 @@ const artist=artistData?.artists[artistId]?.attributes
         <img
           alt="profile"
           src={
-            artistId ? artist.artwork?.url
-              .replace('{w}', '500')
-              .replace('{h}', '500')
+            artistId
+              ? artistData?.attributes?.artwork?.url
+                  .replace("{w}", "500")
+                  .replace("{h}", "500")
               : songData?.images?.coverart
           }
           className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
@@ -22,17 +21,19 @@ const artist=artistData?.artists[artistId]?.attributes
 
         <div className="ml-5">
           <p className="font-bold sm:text-3xl text-xl text-white">
-            {artistId ? artist?.name : songData?.title}
+            {artistId ? artistData?.attributes?.name : songData?.title}
           </p>
           {!artistId && (
             <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
-              <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
+              <p className="text-base text-gray-400 mt-2">
+                {songData?.subtitle}
+              </p>
             </Link>
           )}
 
           <p className="text-base text-gray-400 mt-2">
             {artistId
-              ? artist?.genreNames[0]
+              ? artistData?.attributes?.genreNames[0]
               : songData?.genres?.primary}
           </p>
         </div>
