852121c11b9bc03a6d8cc1260248ed57a68ebf6b
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-22 19:17:25 +0400

    checked sidebar component for correctly operactions

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/logo.webp";
 import { links } from "../../data/links";
