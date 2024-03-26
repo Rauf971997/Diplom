dbc128af546513b365b553443ad3ee9a0297441c
Rauf Baghirov <rauf777bagirov7@gmail.com>
2024-03-26 21:35:29 +0300

    added icon for trending artist

--- src/pages/TrendingArtist/TrendingArtist.jsx
+++ src/pages/TrendingArtist/TrendingArtist.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { FaStar } from 'react-icons/fa';
 import { ArtistCard, Error, Loader } from '../../components';
 import { useGetTopChartsQuery } from '../../redux/services/shazamCore';
 
@@ -12,7 +12,9 @@ const TrendingArtist = () => {
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Trending artists</h2>
+<h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+  <FaStar className="inline-block mr-2" /> Trending artists
+</h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((track) => <ArtistCard key={track.key} track={track} />)}
