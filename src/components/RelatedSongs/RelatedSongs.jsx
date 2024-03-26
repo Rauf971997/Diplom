e88d9e6bd28ebfa4915623d8a8cd3caa6715eaa9
Rauf Baghirov <rauf777bagirov7@gmail.com>
2024-03-26 16:38:22 +0300

    correction of imprint in titles

--- src/components/RelatedSongs/RelatedSongs.jsx
+++ src/components/RelatedSongs/RelatedSongs.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import SongBar from '../SongBar/SongBar';
-import "./RelatedSongs.css";
+// import "./RelatedSongs.css";
 const RelatedSongs = ({ data, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => {
   return (
-    <div className="related-songs-container">
-      <h1 className="related-songs-title ">Related Songs:</h1>
+    <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
 
-      <div className="related-songs-list">
-        {data?.map((song, i) => (
-          <SongBar
-            key={`${song.key}-${artistId}`}
-            song={song}
-            i={i}
-            artistId={artistId}
-            isPlaying={isPlaying}
-            activeSong={activeSong}
-            handlePauseClick={handlePauseClick}
-            handlePlayClick={handlePlayClick}
-          />
-        ))}
-      </div>
+    <div className="mt-6 w-full flex flex-col">
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${artistId}-${song.key}-${i}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+        />
+      ))}
     </div>
+  </div>
   );
 };
 
