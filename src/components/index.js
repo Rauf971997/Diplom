e59cb0d3387d277e348eeb3f7080c029a6df49f4
Rauf Baghirov <rauf777bagirov7@gmail.com>
2024-03-26 14:02:54 +0300

    improved component file structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
 import Sidebar from './Sidebar/Sidebar';
-import Searchbar from './Searchbar';
-import SongCard from './SongCard';
-import TopPlay from './TopPlay';
-import ArtistCard from './ArtistCard';
-import DetailsHeader from './DetailsHeader';
-import SongBar from './SongBar';
-import RelatedSongs from './RelatedSongs';
+import Searchbar from './Searchbar/Searchbar';
+import SongCard from './SongCard/SongCard';
+import TopPlay from './Top/TopPlay';
+import ArtistCard from './ArtistCard/ArtistCard';
+import DetailsHeader from './Header/DetailsHeader';
+import SongBar from './SongBar/SongBar';
+import RelatedSongs from './RelatedSongs/RelatedSongs';
 import MusicPlayer from './MusicPlayer';
-import Loader from './Loader';
-import Error from './Error';
+import Loader from './Loader/Loader';
+import Error from './Error/Error';
 
 export {
   TopPlay,
