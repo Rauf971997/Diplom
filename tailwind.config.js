53af57902004dfb84f7946c69379415372019bd8
Rauf Baghirov <rauf777bagirov7@gmail.com>
2024-03-27 18:11:41 +0300

    changed path for index file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
