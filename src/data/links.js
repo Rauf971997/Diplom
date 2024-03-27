a9809b0b6faef62c5a742e1de63e845fde33b0eb
Rauf Baghirov <rauf777bagirov7@gmail.com>
2024-03-27 17:14:33 +0300

    delete path on sidebar

--- src/data/links.js
+++ src/data/links.js
@@ -1,9 +1,8 @@
-import { BsFillHouseFill as HomeIcon, BsGeoAlt as LocationIcon, BsPeopleFill as ArtistsIcon, BsMusicNoteList as MusicIcon, BsBoxArrowRight as LogoutIcon } from "react-icons/bs";
+import { BsFillHouseFill as HomeIcon, BsGeoAlt as LocationIcon, BsPeopleFill as ArtistsIcon, BsMusicNoteList as MusicIcon } from "react-icons/bs";
 
 export const links = [
   { name: "Explore", to: "/", icon: HomeIcon },
   { name: "Around You", to: "/around-you", icon: LocationIcon },
   { name: "Trending Artists", to: "/top-artists", icon: ArtistsIcon },
-  { name: "Top Hits", to: "/top-charts", icon: MusicIcon },
-  { name: "Logout", to: "/signin", icon: LogoutIcon },
+  { name: "Top Hits", to: "/top-charts", icon: MusicIcon }
 ];
