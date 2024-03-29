f28260149256f619934ae18a2988da6deb68811a
Rauf Baghirov <rauf777bagirov7@gmail.com>
2024-03-29 15:29:58 +0400

    change flex direction for correctness show components on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <div className="content">
         <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Explore />} />
